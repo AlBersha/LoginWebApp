e24da2c59df51290d05ba7cf4a514105c2e8ae49
Oleksandra Bershatska <oleksandra.bershatska@solarwinds.com>
2021-12-10 02:13:13 +0200

    add basic pages and navigation

---
+++ LoginApp/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using LoginApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Registration");
+        }
+
+        public IActionResult RegisterUser(UserModel model)
+        {
+            return View("LoginSuccessfully");
+        }
+    }
+}
---
+++ LoginApp/Models/Services/ISecuritySevice.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace LoginApp.Models.Services
+{
+    public interface ISecuritySevice
+    {
+        public IList<UserModel> Users { get; set; }
+        public bool IsValid(UserModel user);
+    }
+}
